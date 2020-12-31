21dc7b2f1c5aadcbd5d19b8d586f97d5cd0c6855
Tim-Tianyu <tim.wang.tianyu@gmail.com>
2021-01-01 02:20:53 +0800

    run all wdc

--- run_all_wdc.py
+++ run_all_wdc.py
@@ -1,6 +1,5 @@
 datasets = ["all","computers", "cameras", "shoes", "watches"]
-attrs = ['title', 'title_description', 'title_description_brand', 'title_description_brand_specTableContent']
-sizes = ["small", "medium", "large", "xlarge"]
+sizes = ["xlarge"]
 
 import os
 import time
@@ -8,26 +7,27 @@ import time
 gpu_id = 0
 
 for d in datasets:
-    for attr in attrs:
-        for size in sizes:
-            dataset = '_'.join(['wdc', d, attr, size])
-            for dk in [True, False]:
-                for da in [True, False]:
-                    for run_id in range(5):
-                        cmd = """CUDA_VISIBLE_DEVICES=%d python train_ditto.py \
-                          --task %s \
-                          --logdir results_wdc/ \
-                          --fp16 \
-                          --finetuning \
-                          --batch_size 64 \
-                          --lr 3e-5 \
-                          --n_epochs 10 \
-                          --run_id %d""" % (gpu_id, dataset, run_id)
-                        if da:
-                            cmd += ' --da del'
-                        if dk:
-                            cmd += ' --dk product'
-                        if attr != 'title':
-                            cmd += ' --summarize'
-                        print(cmd)
-                        os.system(cmd)
+    for size in sizes:
+        dataset = '_'.join(['wdc', d, size])
+        for probability in [0.0, 0.1, 0.2, 0.4, 0.6]:
+            for run_id in range(5):
+                cmd = """CUDA_VISIBLE_DEVICES=%d python train_ditto.py \
+                    --task %s \
+                    --logdir results_wdc/ \
+                    --fp16 \
+                    --finetuning \
+                    --batch_size 64 \
+                    --lr 3e-5 \
+                    --n_epochs 10 \
+                    --run_id %d \
+                    --test_typing_error %f \
+                    --seed %d \
+                    --summarize""" % (gpu_id, dataset, run_id, probability, run_id)
+                # if da:
+                #     cmd += ' --da del'
+                # if dk:
+                #     cmd += ' --dk product'
+                # if attr != 'title':
+                #     cmd += ' --summarize'
+                print(cmd)
+                #os.system(cmd)
